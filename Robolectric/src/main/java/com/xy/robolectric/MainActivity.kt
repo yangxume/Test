3234f841881a432662616cab5a486fedc8f3944e
xuyang <xuyangme@126.com>
2019-01-25 20:44:12 +0800

    LoginTest add

--- Robolectric/src/main/java/com/xy/robolectric/MainActivity.kt
+++ Robolectric/src/main/java/com/xy/robolectric/MainActivity.kt
@@ -3,6 +3,11 @@ package com.xy.robolectric
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+//https://blog.csdn.net/qq_28190653/article/details/81035835
+
+//https://blog.csdn.net/u010316858/article/details/70312795
+
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
