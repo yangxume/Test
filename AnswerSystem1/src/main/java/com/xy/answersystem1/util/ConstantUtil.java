7d71b4eb48ce4d5c51136fedb886ed69a5b4359c
xuyang <xuyangme@126.com>
2019-02-28 18:52:30 +0800

    answer system refactor

---
+++ AnswerSystem1/src/main/java/com/xy/answersystem1/util/ConstantUtil.java
@@ -0,0 +1,13 @@
+package com.xy.answersystem1.util;
+
+
+/**
+ * 配置类
+ * 
+ * @author 金钟焕
+ */
+public class ConstantUtil {
+	
+	public static final String isCorrect="1";//正确
+	public static final String isError="0";//错误
+}
