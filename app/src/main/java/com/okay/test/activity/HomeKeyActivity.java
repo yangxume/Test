d2f82516c469dccf3a08e81ab7c469255ff2e106
xuyang <xuyangme@126.com>
2019-03-13 18:52:07 +0800

    home 键监听测试

---
+++ app/src/main/java/com/okay/test/activity/HomeKeyActivity.java
@@ -0,0 +1,93 @@
+package com.okay.test.activity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.okay.test.R;
+
+public class HomeKeyActivity extends AppCompatActivity {
+
+
+    private static final String LOG_TAG = "HomeKeyActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_processisbackground);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerHomeKeyReceiver(this);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterHomeKeyReceiver(this);
+    }
+
+    private HomeWatcherReceiver mHomeKeyReceiver = null;
+
+    private void registerHomeKeyReceiver(Context context) {
+        Log.i(LOG_TAG, "registerHomeKeyReceiver");
+        mHomeKeyReceiver = new HomeWatcherReceiver();
+        final IntentFilter homeFilter = new IntentFilter(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
+
+        context.registerReceiver(mHomeKeyReceiver, homeFilter);
+    }
+
+    private void unregisterHomeKeyReceiver(Context context) {
+        Log.i(LOG_TAG, "unregisterHomeKeyReceiver");
+        if (null != mHomeKeyReceiver) {
+            context.unregisterReceiver(mHomeKeyReceiver);
+        }
+    }
+
+    class HomeWatcherReceiver extends BroadcastReceiver {
+
+        private static final String LOG_TAG = "HomeReceiver";
+        private static final String SYSTEM_DIALOG_REASON_KEY = "reason";
+        private static final String SYSTEM_DIALOG_REASON_RECENT_APPS = "recentapps";
+        private static final String SYSTEM_DIALOG_REASON_HOME_KEY = "homekey";
+        private static final String SYSTEM_DIALOG_REASON_LOCK = "lock";
+        private static final String SYSTEM_DIALOG_REASON_ASSIST = "assist";
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            Log.i(LOG_TAG, "onReceive: action: " + action);
+            if (action.equals(Intent.ACTION_CLOSE_SYSTEM_DIALOGS)) {
+                // android.intent.action.CLOSE_SYSTEM_DIALOGS
+                String reason = intent.getStringExtra(SYSTEM_DIALOG_REASON_KEY);
+                Log.i(LOG_TAG, "reason: " + reason);
+
+                if (SYSTEM_DIALOG_REASON_HOME_KEY.equals(reason)) {
+                    // 短按Home键
+                    Log.i(LOG_TAG, "homekey");
+
+                } else if (SYSTEM_DIALOG_REASON_RECENT_APPS.equals(reason)) {
+                    // 长按Home键 或者 activity切换键
+                    Log.i(LOG_TAG, "long press home key or activity switch");
+
+                } else if (SYSTEM_DIALOG_REASON_LOCK.equals(reason)) {
+                    // 锁屏
+                    Log.i(LOG_TAG, "lock");
+                } else if (SYSTEM_DIALOG_REASON_ASSIST.equals(reason)) {
+                    // samsung 长按Home键
+                    Log.i(LOG_TAG, "assist");
+                }
+
+            }
+        }
+
+    }
+}
