6ebac9edec7ad3fb0d3e54c17bfeec72ad48a395
xuyang <xuyangme@126.com>
2019-09-03 17:17:41 +0800

    布局优化之ViewStub

--- t_layout/src/main/java/com/xy/t_layout/activity/ActivityUIOptimize.java
+++ t_layout/src/main/java/com/xy/t_layout/activity/ActivityUIOptimize.java
@@ -3,6 +3,8 @@ package com.xy.t_layout.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewStub;
 
 import com.xy.t_layout.R;
 
@@ -20,6 +22,18 @@ public class ActivityUIOptimize extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ui_optimize);
+//        setContentView(R.layout.activity_ui_optimize);
+//        setContentView(R.layout.activity_ui_optimize_include);
+//        setContentView(R.layout.activity_ui_optimize_merge);
+
+        setContentView(R.layout.activity_ui_optimize_viewstub);
+        findViewById(R.id.btn_test_viewstub).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ViewStub viewstub_loading = findViewById(R.id.viewstub_loading);
+                viewstub_loading.inflate();
+            }
+        });
+
     }
 }
