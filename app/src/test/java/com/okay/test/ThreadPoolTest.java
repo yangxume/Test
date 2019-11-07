cc2fea34dbb1c04319733b7dd43722718f19de42
xuyang <xuyangme@126.com>
2019-11-07 19:31:34 +0800

    test. ScheduledThreadPoolExecutor

---
+++ app/src/test/java/com/okay/test/ThreadPoolTest.java
@@ -0,0 +1,24 @@
+package com.okay.test;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+
+/**
+ * @Author: yangxume@gmail.com
+ * @Time: 2019-11-07 18:13
+ * @Desc:
+ *
+ * 使用线程池执行定时任务
+ * https://segmentfault.com/a/1190000008038848
+ *
+ * https://www.cnblogs.com/fpqi/p/9720014.html
+ *
+ */
+public class ThreadPoolTest {
+
+
+    ScheduledThreadPoolExecutor scheduledThreadPoolExecutor ;
+
+//    public void
+
+
+}
