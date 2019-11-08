b5439d706ccb308a386f51de6eb18829fb5ea43c
xuyang <xuyangme@126.com>
2019-11-08 17:22:07 +0800

    ASMTest

---
+++ app/src/test/java/com/okay/test/AsmTest.java
@@ -0,0 +1,23 @@
+package com.okay.test;
+
+/**
+ * @Author: yangxume@gmail.com
+ * @Time: 2019-11-08 11:34
+ * @Desc:
+ *
+ * https://my.oschina.net/ta8210/blog/162796
+ *
+ * https://www.jianshu.com/p/b5ffe845fe2d
+ *
+ */
+public class AsmTest {
+
+    public void printLog(){
+
+        System.out.print("printLog");
+
+    }
+
+
+
+}
