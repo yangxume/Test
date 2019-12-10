2e41db69835003603984b6e6685503ac3c1f6622
xuyang <xuyangme@126.com>
2019-12-10 16:32:34 +0800

    【add】study shape

---
+++ app/src/main/java/com/okay/test/TestShpeActivity.java
@@ -0,0 +1,33 @@
+package com.okay.test;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.okay.test.utils.DeviceUtil;
+import com.okay.test.utils.LogUtils;
+
+/**
+ * @Author: yangxume@gmail.com
+ * @Time: 2019-09-09 17:04
+ * @Desc:
+ */
+public class TestShpeActivity extends AppCompatActivity {
+
+    private static final String TAG = TestShpeActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_test_shape);
+
+        long t1 = System.currentTimeMillis();
+
+        DeviceUtil.getIMEI();
+
+        long t2 = System.currentTimeMillis();
+
+        LogUtils.d(TAG, " t2 - t1  = "+(t2-t1));
+    }
+}
