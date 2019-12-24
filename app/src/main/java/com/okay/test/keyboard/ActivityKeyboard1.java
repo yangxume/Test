ab85b1cdea6b5560d3135c0a821c9204e51db1ff
xuyang <xuyangme@126.com>
2019-12-24 15:36:35 +0800

    【add】         android:scrollbarSize="6dp"

--- app/src/main/java/com/okay/test/keyboard/ActivityKeyboard1.java
+++ app/src/main/java/com/okay/test/keyboard/ActivityKeyboard1.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 import com.okay.test.R;
 
@@ -14,17 +16,22 @@ import com.okay.test.R;
  * @Time: 2019-11-21 11:01
  * @Desc:
  */
-public class ActivityKeyboard1 extends AppCompatActivity {
+public class ActivityKeyboard1 extends AppCompatActivity implements View.OnLayoutChangeListener{
 
-    private LinearLayout parent;
-    private EditText et_account;
-    private NestedScrollView scroll;
-
+    private ScrollView scrollview;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_keyboard_1);
+        scrollview = (ScrollView) findViewById(R.id.scrollview);
+        scrollview.addOnLayoutChangeListener(this);
+    }
+
+    @Override
+    public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+
+        scrollview.scrollTo(0,bottom);
 
     }
 }
